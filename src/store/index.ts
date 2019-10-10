06ded778ab1f576051929bc067d45a0426974838
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-10-10 23:36:23 +0300

    Логирование

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { playerReducer } from "../reducers/player";
+import logger from "redux-logger";
 
 const reducers = combineReducers({
   player: playerReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger));
 export default store;
