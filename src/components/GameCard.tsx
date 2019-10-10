e6984ae5cf0e50b9a98c89d7cd5729d03262ee48
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-10-10 04:19:57 +0300

    Добавлена верстка для различных состояний игры

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,9 +7,55 @@ type Props = {
 };
 
 const GameCard: React.FC<Props> = ({ game }) => {
+  const isWinner = (playerName: string, game: GameInfo) => {
+    return (
+      (game.gameResult === "owner" || game.gameResult === "opponent") &&
+      playerName === game[game.gameResult]
+    );
+  };
+
+  const getCardTheme = (game: GameInfo) => {
+    return game.state === "ready"
+      ? "game-card_theme_gray"
+      : "game-card_theme_light-gray";
+  };
+
+  const getPlayerNameTheme = (playerName: string, game: GameInfo) => {
+    if (isWinner(playerName, game)) {
+      return "game-card__player-name_theme_won";
+    }
+    if (game.gameResult === "draw") {
+      return "game-card__player-name_theme_drew";
+    }
+    return "game-card__player-name_theme_default";
+  };
+
+  const renderPlayerName = (playerName: string, game: GameInfo) => {
+    const classes = [
+      "game-card__player-name",
+      getPlayerNameTheme(playerName, game)
+    ].join(" ");
+    return (
+      <div className={classes}>
+        <p className="game-card__player-name-text">{playerName}</p>
+        {isWinner(playerName, game) && (
+          <p className="game-card__player-name-text">&#10004;</p>
+        )}
+      </div>
+    );
+  };
+
+  const cardClasses = ["game-card", getCardTheme(game)].join(" ");
+
   return (
-    <div className="game-card">
-      <p className="game-card__player-name">{game.owner}</p>
+    <div className={cardClasses}>
+      {renderPlayerName(game.owner, game)}
+      {game.state !== "ready" && game.owner && (
+        <>
+          <hr className="game-card__separator" />
+          {renderPlayerName(game.opponent, game)}
+        </>
+      )}
       <p className="game-card__time">00:05:32</p>
     </div>
   );
