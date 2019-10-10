cad095821e6d68f5e2e06348af66af935e9e7b98
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-10-10 17:32:49 +0300

    Добавлена верстка кнопки создания игры

---
+++ src/components/AddGameButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AddGameButton.css";
+
+const AddGameButton: React.FC = () => {
+  return (
+    <button className="fab" type="button">
+      +
+    </button>
+  );
+};
+
+export default AddGameButton;
--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MainLayout from "./MainLayout";
 import GameListContainer from "../../containers/GameListContainer";
+import AddGameButton from "../AddGameButton";
 
 const HomeLayout = () => {
   return (
     <MainLayout>
       <GameListContainer />
+      <AddGameButton />
     </MainLayout>
   );
 };
