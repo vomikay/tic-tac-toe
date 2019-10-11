334182ecd7dc167ae951263d570bac798cc18435
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-10-11 22:20:22 +0300

    Рефакторинг

--- src/components/PlayerNameInput.tsx
+++ src/components/PlayerNameInput.tsx
@@ -5,14 +5,14 @@ type Props = {
   onChange: (name: string) => void;
 };
 
-const PlayerNameInput: React.FC<Props> = ({ onChange: onInput }) => {
+const PlayerNameInput: React.FC<Props> = ({ onChange }) => {
   return (
     <div className="player-name-input">
       <input
         className="player-name-input__editor"
         type="text"
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          onInput(e.target.value)
+          onChange(e.target.value)
         }
       />
       <span className="player-name-input__highlight"></span>
