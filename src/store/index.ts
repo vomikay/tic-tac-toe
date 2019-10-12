1e8bd567ff4cf33287371f836640bcf0e12c4c9f
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-10-12 14:56:26 +0300

    Синхронизация вкладок

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,25 @@
 import { createStore, applyMiddleware, Middleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "../reducers";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  PERSIST,
+  REHYDRATE
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
+import { createStateSyncMiddleware } from "redux-state-sync";
+import { INPUT_USER_NAME } from "../actions/user";
 
-const middlewares: Middleware[] = [thunk];
+const syncConfig = {
+  blacklist: [PERSIST, REHYDRATE, INPUT_USER_NAME]
+};
+
+const middlewares: Middleware[] = [
+  thunk,
+  createStateSyncMiddleware(syncConfig)
+];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
