3b9d5693c3103ad60b79bf9d6a82305c37a9ab67
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-10-15 09:03:08 +0300

    Исправление ошибки имени пользователя

--- src/components/home/UserNameInput.tsx
+++ src/components/home/UserNameInput.tsx
@@ -2,14 +2,16 @@ import React, { ChangeEvent } from "react";
 import { TextField } from "@material-ui/core";
 
 type Props = {
+  value: string;
   onChange: (name: string) => void;
 };
 
-const UserNameInput: React.FC<Props> = ({ onChange }) => {
+const UserNameInput: React.FC<Props> = ({ value, onChange }) => {
   return (
     <TextField
       placeholder="Введите имя"
       margin="normal"
+      value={value}
       onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
     />
   );
--- src/containers/home/UserNameInputContainer.tsx
+++ src/containers/home/UserNameInputContainer.tsx
@@ -1,9 +1,10 @@
 import { connect } from "react-redux";
 import { inputUserName } from "../../actions/user";
 import UserNameInput from "../../components/home/UserNameInput";
+import { AppState } from "../../store";
 
 const UserNameInputContainer = connect(
-  null,
+  (state: AppState) => ({ value: state.user.name }),
   { onChange: inputUserName }
 )(UserNameInput);
 
